f289e2606c077401eef3a980f4b38db6f204ecae
Maurycy Szmuc <mauryc_s@wp.pl>
2021-05-26 14:47:35 +0200

    Added onUpdate, onActivated, and checkBanned

--- background.js
+++ background.js
@@ -1,6 +1,28 @@
 console.log("background.js")
+let data = JSON.parse(localStorage.getItem("banned_sites"));
+if(!data){
+    data=[];
+} 
+
+chrome.tabs.onUpdated.addListener(
+    function(tabId,changeInfo,tab) {
+      if (changeInfo.url) {
+        console.log("OnUpdated: "+changeInfo.url);
+        checkBanned(changeInfo.url);
+      }
+    }
+  );
 chrome.tabs.onActivated.addListener(tab => {
     chrome.tabs.get(tab.tabId,current_tab_info=>{
-        console.log(tab.tabId,current_tab_info.url)
+        console.log("OnActivated: "+current_tab_info.url);
+        checkBanned(current_tab_info.url);
     });
 });
+
+function checkBanned(url){
+    data.forEach(element => {
+        if (url == element){
+            console.log("I'm banned!")
+        }
+    });
+}
