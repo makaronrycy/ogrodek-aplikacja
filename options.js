957caea84848a5b2acd9c3c6a9e8798f6e5d0299
Axomi <83697500+Axomi24@users.noreply.github.com>
2021-05-26 15:49:08 +0200

    Adding banned sites

--- options.js
+++ options.js
@@ -1,31 +1,49 @@
 let store;
-function save_options() {
-    var select = document.getElementById("addTrack");
-    var color = select.children[select.selectedIndex].value;
-    localStorage["favorite_color"] = color;
-  
-    // Update status to let user know options were saved.
-    var status = document.getElementById("status");
-    status.innerHTML = "Options Saved.";
-    setTimeout(function() {
-      status.innerHTML = "";
-    }, 750);
+function save_options() 
+{
+  var select = document.getElementById("urlTrack");
+  if (localStorage.getItem("banned_sites"))
+  {
+    var bannedSites = JSON.parse(localStorage.getItem("banned_sites"));
   }
-  
-  // Restores select box state to saved value from localStorage.
-  function restore_options() {
-    var favorite = localStorage["favorite_color"];
-    if (!favorite) {
-      return;
-    }
-    var select = document.getElementById("color");
-    for (var i = 0; i < select.children.length; i++) {
-      var child = select.children[i];
-      if (child.value == favorite) {
-        child.selected = "true";
-        break;
-      }
-    }
+  else
+  {
+    var bannedSites = [];
   }
-  document.addEventListener('DOMContentLoaded', restore_options);
-  document.querySelector('#save').addEventListener('click', save_options);
+  bannedSites.push(select.value);
+  localStorage.setItem('banned_sites', JSON.stringify(bannedSites));
+  
+  // Update status to let user know options were saved.
+  var status = document.getElementById("status");
+  status.innerHTML = "Options Saved.";
+  setTimeout(function() {
+    status.innerHTML = "";
+  }, 2000);
+  show_options();
+}
+
+// Restores select box state to saved value from localStorage.
+function show_options() {
+  var sites = JSON.parse(localStorage.getItem('banned_sites'));
+  if (!sites) 
+  {
+    return;
+  }
+  document.getElementById("trackList").innerHTML='';
+  for (var i = 0; i < sites.length; i++) 
+  {
+    var node = document.createElement("LI");               
+    var textnode = document.createTextNode(sites[i]);         
+    node.appendChild(textnode);                              
+    document.getElementById("trackList").appendChild(node);     
+  }
+}
+
+window.onload = function()
+{
+  document.getElementById("addTrack").addEventListener('click', save_options);
+  show_options();
+}  
+  
+  
+ 
