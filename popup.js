a9ef520d742228599ab1554b28763c19d34ccecc
Maurycy Szmuc <mauryc_s@wp.pl>
2021-05-28 12:26:23 +0200

    Deleted parameter days because redundant

--- popup.js
+++ popup.js
@@ -79,11 +79,10 @@ window.onload = function(){
                     plant.finishGrowing();
                     return;
                 }
-                const days = Math.floor(distance / (1000 * 60 * 60 * 24));
                 const hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
                 const minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
                 const seconds = Math.floor((distance % (1000 * 60)) / 1000);
-                document.getElementById("demo").innerHTML = days + "d " + hours + "h "
+                document.getElementById("demo").innerHTML = hours + "h "
                 + minutes + "m " + seconds + "s ";
                 plant.saveToStorage();
                 plant.updateImg();
